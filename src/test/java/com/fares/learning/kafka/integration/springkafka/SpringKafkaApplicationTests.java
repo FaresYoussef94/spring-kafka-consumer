c466bd0321cbf570719fce4e015348f959ce3d6e
Fares Youssef <fares.youssef@seal-software.com>
2020-01-11 14:46:52 +0200

    spring initializer

---
+++ src/test/java/com/fares/learning/kafka/integration/springkafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fares.learning.kafka.integration.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
